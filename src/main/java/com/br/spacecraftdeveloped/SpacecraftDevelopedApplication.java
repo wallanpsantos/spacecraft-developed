8a2be4c33287e80f478f8f3ee51b94bbc121f370
wallanpsantos <wallanpereira09@gmail.com>
2023-05-20 20:09:04 -0300

    Projeto Base

---
+++ src/main/java/com/br/spacecraftdeveloped/SpacecraftDevelopedApplication.java
@@ -0,0 +1,13 @@
+package com.br.spacecraftdeveloped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacecraftDevelopedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpacecraftDevelopedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/spacecraftdeveloped/SpacecraftDevelopedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.spacecraftdeveloped;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacecraftDevelopedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
