86d3d1d9e685d1280d55ae89139ba231b318c1cc
wallapsantos <wallanpereira09@gmail.com>
2023-05-21 22:49:37 -0300

    Added: @JsonNaming(SnakeCaseStrategy.class)

--- src/main/java/com/br/spacecraftdeveloped/dataprovider/model/response/HeatShield.java
+++ src/main/java/com/br/spacecraftdeveloped/dataprovider/model/response/HeatShield.java
@@ -1,15 +1,16 @@
 package com.br.spacecraftdeveloped.dataprovider.model.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.math.BigDecimal;
+
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 
-@JsonInclude(Include.NON_EMPTY)
 @JsonNaming(SnakeCaseStrategy.class)
-public record DragonResponse(
-        String id,
-        String name,
-        HeatShield heatShield) {
+public record HeatShield(
+        String material,
+        BigDecimal sizeMeters,
+        BigDecimal tempDegrees,
+        String devPartner
+) {
 }
